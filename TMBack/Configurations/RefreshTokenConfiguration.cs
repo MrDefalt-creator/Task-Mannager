a8dd10274a590459ef5fffe7dc909c8876b37877
Ivan <defalt06@mail.ru>
2025-01-15 17:08:44 +0300

    Сreated all configuration.

---
+++ TMBack/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TMBack.Models;
+
+namespace TMBack.Configurations
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshTokenEntity>
+    {
+        public void Configure(EntityTypeBuilder<RefreshTokenEntity> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder
+                .HasOne(t => t.User)
+                .WithMany(r => r.RefreshTokens)
+                .HasForeignKey(r => r.UserId);
+        }
+    }
+}
---
+++ TMBack/Configurations/TaskConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TMBack.Models;
+
+namespace TMBack.Configurations
+{
+    public class TaskConfiguration : IEntityTypeConfiguration<TaskEntity>
+    {
+        public void Configure(EntityTypeBuilder<TaskEntity> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder
+                .HasOne(t => t.User)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(u => u.UserId); 
+        }
+    }
+}
