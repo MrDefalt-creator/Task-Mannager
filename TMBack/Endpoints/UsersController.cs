3b1af1ea3d64d8df81b4c90f2e281787df3a5217
Ivan <defalt06@mail.ru>
2025-02-06 17:03:24 +0300

    Renamed Endpoints

---
+++ TMBack/Endpoints/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using TMBack.Contracts.User;
+using TMBack.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TMBack.Endpoints;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly UsersService _usersService;
+
+    public UsersController(UsersService usersService)
+    {
+        _usersService = usersService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        await _usersService.Register(request.UserName, request.Email, request.Password);
+
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginUserRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var token = await _usersService.Login(request.Email, request.Password);
+
+        return Ok(token);
+    }
+}
