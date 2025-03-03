0cf154eb96d1b2a4a9734f2bc5b8ead5c846c74f
Ivan <defalt06@mail.ru>
2025-03-03 21:23:35 +0300

    needed to create thunk for update and delete and rewrite something in TaskInfo.jsx

--- Frontend/src/components/dashboard_page/TaskInfo.jsx
+++ Frontend/src/components/dashboard_page/TaskInfo.jsx
@@ -1,4 +1,7 @@
-﻿export default function TaskInfo(){
+﻿import {useSelector} from "react-redux";
+
+export default function TaskInfo(){
+    const {taskId, title, description, createdAt,mustFinish} = useSelector((state) => state.task);
     return(
         <section className='block w-full h-full'>
             <div className='flex justify-center mt-4 items-center'>
@@ -6,17 +9,20 @@
             </div>
             <div className='flex justify-around mt-4'>
                 <div/>
-                <p className='font-sans font-light text-base text-slate-600'>ID:</p>
+                <p className='font-sans font-light text-base text-slate-600'>ID: {taskId}</p>
             </div>
-            <div className='px-7'>
-               <h2 className='font-sans font-medium text-3xl'>TITLE</h2>
+            <div className='flex justify-center mt-5'>
+               <h2 className='font-sans font-medium text-3xl'>{title}</h2>
             </div>
             <div className='flex justify-around mt-6 px-7 items-center'>
-                <p className='font-sans text-xl font-medium'>Создана в: </p>
-                <p className='font-sans text-xl font-medium'>Необходимо завершить: </p>
+                <p className='font-sans text-xl font-medium'>Создана в: {createdAt}</p>
+                <p className='font-sans text-xl font-medium'>Необходимо завершить: {mustFinish}</p>
             </div>
             <div className='flex justify-center h-full w-full mt-8'>
-                <input className='w-[80%] h-full border-2 border-slate-400 focus:outline-none' type='text'/>
+                <textarea
+                    className='font-sans font-medium text-base w-[80%] border-2 border-slate-400 focus:outline-none h-[618px] resize-none'
+
+                >{description}</textarea>
             </div>
         </section>
     )
